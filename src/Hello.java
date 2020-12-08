5554b412025d8659d4f7b225df3cd23a20563cb2
creattech <crystalsmcneil@gmail.com>
2020-12-07 22:08:29 -0600

    adds three variables

--- src/Hello.java
+++ src/Hello.java
@@ -3,8 +3,13 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Crystal");
 
-        int firstNumber = 5;
-
+        int firstNumber = (10 + 5) + (2 * 10);
         System.out.println(firstNumber);
+
+        int secondNumber = 12;
+        System.out.println(secondNumber);
+
+        int thirdNumber = 6;
+        System.out.println(thirdNumber);
     }
 }
