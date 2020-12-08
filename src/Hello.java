b77effd0e8d638c65cf8dd1a9522ddbb8feba21f
creattech <crystalsmcneil@gmail.com>
2020-12-07 20:27:04 -0600

    basic output

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Crystal");
+
+        int firstNumber = 5;
+
+        System.out.println(firstNumber);
+    }
+}
