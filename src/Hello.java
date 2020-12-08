26dbcdde3293c6b6438d691a73b222b8320553e2
creattech <crystalsmcneil@gmail.com>
2020-12-07 22:47:22 -0600

    adds new sum

--- src/Hello.java
+++ src/Hello.java
@@ -4,12 +4,9 @@ public class Hello {
         System.out.println("Hello, Crystal");
 
         int firstNumber = (10 + 5) + (2 * 10);
-        System.out.println(firstNumber);
-
         int secondNumber = 12;
-        System.out.println(secondNumber);
-
-        int thirdNumber = 6;
-        System.out.println(thirdNumber);
+        int thirdNumber = firstNumber * 2;
+        int total = firstNumber + secondNumber + thirdNumber;
+        System.out.println(total);
     }
 }
